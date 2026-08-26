1d1e35d3fc23cca0f1536a5538ef3004d63aeda8
[dyad] <ptharumjaya2005@gmail.com>
2026-08-26 15:31:52 +0700

    [dyad] Mengganti logo Hero dengan versi transparan terbaru - wrote 2 file(s)

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,12 +13,12 @@ const Hero = () => {
         <div className="absolute inset-0 bg-black/10"></div>
       </div>
 
-      {/* Logo Overlay - Menggunakan logo baru dengan transparansi */}
-      <div className="relative z-10 animate-in fade-in zoom-in duration-1000 flex items-center justify-center">
+      {/* Logo Overlay - Menggunakan logo baru tanpa background */}
+      <div className="relative z-10 animate-in fade-in zoom-in duration-1000 flex items-center justify-center p-4">
         <img 
           src="/logo_hd.png" 
           alt="CV IM Logo" 
-          className="w-64 sm:w-80 md:w-[450px] h-auto object-contain drop-shadow-2xl"
+          className="w-72 sm:w-96 md:w-[500px] h-auto object-contain drop-shadow-[0_20px_50px_rgba(0,0,0,0.3)]"
         />
       </div>
     </div>
