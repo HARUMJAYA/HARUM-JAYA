9d304318dda88d17a0c663dbe465498bfd6baa6b
[dyad] <ptharumjaya2005@gmail.com>
2026-08-26 11:51:31 +0700

    [dyad] Menambahkan logo HD yang diunggah ke tengah bagian Hero. - wrote 2 file(s)

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,8 @@
-import { ChevronRight } from "lucide-react";
-import { Link } from "react-router-dom";
+import React from "react";
 
 const Hero = () => {
   return (
-    <div className="relative w-full h-[450px] sm:h-[550px] md:h-[650px] overflow-hidden">
+    <div className="relative w-full h-[450px] sm:h-[550px] md:h-[650px] overflow-hidden flex items-center justify-center">
       {/* Background Image */}
       <div 
         className="absolute inset-0 bg-cover bg-center"
@@ -11,10 +10,17 @@ const Hero = () => {
           backgroundImage: `url('/hero-bg.jpg')`,
         }}
       >
-        <div className="absolute inset-0 bg-black/10"></div>
+        <div className="absolute inset-0 bg-black/20"></div>
       </div>
 
-      {/* Konten Hero telah dihapus sesuai permintaan */}
+      {/* Logo Overlay */}
+      <div className="relative z-10 animate-in fade-in zoom-in duration-1000">
+        <img 
+          src="/logo_hd.png" 
+          alt="CV IM Logo" 
+          className="w-48 sm:w-64 md:w-80 h-auto object-contain drop-shadow-2xl"
+        />
+      </div>
     </div>
   );
 };
